1ed6f9a31afd2db7f2a0b71fc889d743ed5d90a2
MrBird <852252810@qq.com>
2020-04-19 10:30:06 +0800

    2.0-RELEASE

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,7 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: 'https://localhost:8301/',
+        target: 'http://localhost:8301/',
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
